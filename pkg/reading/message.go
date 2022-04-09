3f2c3b724379568b0e1c17cf513ff7b692ad2079
Crandel <4005032+Crandel@users.noreply.github.com>
2022-04-09 19:24:51 +0200

    fieldalignment

--- pkg/reading/message.go
+++ pkg/reading/message.go
@@ -1,7 +1,7 @@
 package reading
 
 type Message struct {
-	ID      int    `json:"id"`
 	UserId  UserId `json:"user_id"`
 	Payload string `json:"payload"`
+	ID      int    `json:"id"`
 }
--- pkg/reading/room.go
+++ pkg/reading/room.go
@@ -1,6 +1,6 @@
 package reading
 
 type Room struct {
-	Name     string               `json:"name"`
 	Messages map[UserId][]Message `json:"messages"`
+	Name     string               `json:"name"`
 }
